5a9a46d2e3bdf28e71f58d182c5cf02ab0ef6d7e
Apple chen <appletabby@gmail.com>
2019-11-26 18:21:15 +0800

    Install omniauth gem and add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_014857) do
+ActiveRecord::Schema.define(version: 2019_11_26_101452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
